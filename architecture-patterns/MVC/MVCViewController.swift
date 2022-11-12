18b5b38b2f33bdc0d51967307cbf1933023c09d3
danovise <novik301199@gmail.com>
2022-11-12 16:07:21 +0300

    build (app): create new project

---
+++ architecture-patterns/MVC/MVCViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  architecture-patterns
+//
+//  Created by Daria Sechko on 7.11.22.
+//
+
+import UIKit
+
+class MVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
