5fd62d398f185dc82222301db1e1344e3f72a47b
danovise <novik301199@gmail.com>
2022-11-14 17:48:57 +0300

    style (app): creating MVVM file

---
+++ architecture-patterns/MVVM/MVVMModuleAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  MVVMModuleAssembly.swift
+//  architecture-patterns
+//
+//  Created by Daria Sechko on 14.11.22.
+//
+
+import Foundation
---
+++ architecture-patterns/MVVM/MVVMViewModelInput.swift
@@ -0,0 +1,8 @@
+//
+//  MVVMViewModelInput.swift
+//  architecture-patterns
+//
+//  Created by Daria Sechko on 14.11.22.
+//
+
+import Foundation
