357df3ad2356ddf0e2a86673fc99ffe493f16773
danovise <novik301199@gmail.com>
2022-11-15 21:40:01 +0300

    build(app): adding VIPERViewController

--- architecture-patterns/VIPER/VIPERViewController.swift
+++ architecture-patterns/VIPER/VIPERViewController.swift
@@ -7,23 +7,89 @@
 
 import UIKit
 
-class VIPERViewController: UIViewController {
+protocol VIPERViewInput: AnyObject {
+    func showPincode(_ value: String)
+}
 
+protocol VIPERViewOutput: AnyObject {
+    func didPressPincodeButton()
+}
+
+class VIPERViewController: UIViewController  {
+    
+    var output: VIPERViewOutput!
+    
+    private lazy var pincodeLabel: UILabel = {
+        var label = UILabel()
+        
+        label.text = "- - - -"
+        label.textAlignment = .center
+        label.backgroundColor = .lightGray.withAlphaComponent(0.5)
+        
+        return label
+    }()
+    
+    private lazy var receivePincodeButton: UIButton = {
+        var button = UIButton()
+        
+        button.setTitleColor(.blue, for: .normal)
+        button.setTitle("Получить Пин-код", for: .normal)
+        button.backgroundColor = .lightGray.withAlphaComponent(0.5)
+        button.addTarget(self, action: #selector(didPressPincodeButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Private
+    private func setupViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(pincodeLabel)
+        view.addSubview(receivePincodeButton)
     }
-    */
-
+    
+    //MARK: - Actions
+    
+    @objc private func didPressPincodeButton() {
+        output.didPressPincodeButton()
+    }
 }
+
+extension VIPERViewController: VIPERViewInput {
+    
+    func showPincode(_ value: String) {
+        pincodeLabel.text = value
+    }
+}
+
+extension VIPERViewController {
+    
+    class Layout {
+        static var topHeaderOffset = 50
+        static var topOffset = 20
+    }
+    
+    private func setupConstraints() {
+        
+        pincodeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(Layout.topHeaderOffset)
+        }
+        
+        receivePincodeButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(pincodeLabel.snp.bottom).offset(Layout.topOffset)
+        }
+    }
+}
+
+
+
+
+
