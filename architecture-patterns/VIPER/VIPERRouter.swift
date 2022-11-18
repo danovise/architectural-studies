4efe1ca6cd91501cb0dee515cd60e366b576e440
danovise <novik301199@gmail.com>
2022-11-18 19:13:24 +0300

    build (app): adding VIPERRouter

--- architecture-patterns/VIPER/VIPERRouter.swift
+++ architecture-patterns/VIPER/VIPERRouter.swift
@@ -5,4 +5,28 @@
 //  Created by Daria Sechko on 15.11.22.
 //
 
-import Foundation
+import UIKit
+
+protocol VIPERRouterInput {
+    func showPincodeAlert(_ pincode: String)
+    func showDetailScreen()
+}
+
+class VIPERRouter: VIPERRouterInput {
+    
+    weak var view: UIViewController!
+    
+    func showPincodeAlert(_ pincode: String) {
+        let alert = UIAlertController(title: "Пин-код получен \(pincode)", message: nil, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Хорошо", style: .default, handler: nil)
+        alert.addAction(okButton)
+        
+        view.present(alert, animated: true, completion: nil)
+    }
+    
+    func showDetailScreen() {
+        let viewController = MVPViewController.init()
+        view.present(viewController, animated: true)
+    }
+}
+
