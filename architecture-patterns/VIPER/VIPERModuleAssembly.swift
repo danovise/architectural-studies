c59e527a1591b00fb7f58e609b1997e90d7f1ef4
danovise <novik301199@gmail.com>
2022-11-20 22:41:05 +0300

    build(app): adding VIPERModuleAssembly

--- architecture-patterns/VIPER/VIPERModuleAssembly.swift
+++ architecture-patterns/VIPER/VIPERModuleAssembly.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class VIPERModuleAssembly {
+    
+    static func assembly() -> VIPERViewController {
+        
+        let presenter = VIPERPresenter()
+        
+        let viewController = VIPERViewController()
+        viewController.output = presenter
+        
+        presenter.view = viewController
+        
+        let interactor = VIPERInteractor()
+        presenter.interactor = interactor
+        
+        let service = PincodeServiceImpl()
+        interactor.output = presenter
+        interactor.service = service
+        
+        let router = VIPERRouter()
+        presenter.router = router
+        router.view = viewController
+        
+        return viewController
+    }
+}
+
